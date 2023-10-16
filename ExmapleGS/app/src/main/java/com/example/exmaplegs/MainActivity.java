00f20a8b17ecc886919976acc213ed878ee1b388
kimgyeomson <122075249+kimgyeomson@users.noreply.github.com>
2023-10-17 03:51:45 +0900

    example

---
+++ ExmapleGS/app/src/main/java/com/example/exmaplegs/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.exmaplegs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ScrollView;
+
+public class MainActivity extends AppCompatActivity {
+    ScrollView scrollView;
+    ScrollView scrollView2;
+    ImageView imageView;
+    ImageView imageView2;
+    BitmapDrawable bitmap;
+    BitmapDrawable bitmap2;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        scrollView
+    }
+}
