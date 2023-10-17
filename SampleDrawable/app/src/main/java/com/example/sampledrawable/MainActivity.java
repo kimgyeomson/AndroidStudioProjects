0c7399ecacb3f309881ddcb1ec89b26729445311
kimgyeomson <122075249+kimgyeomson@users.noreply.github.com>
2023-10-17 22:54:35 +0900

    Drawable

--- SampleDrawable/app/src/main/java/com/example/sampledrawable/MainActivity.java
+++ SampleDrawable/app/src/main/java/com/example/sampledrawable/MainActivity.java
@@ -1,20 +1,14 @@
-package com.example.widget_;
+package com.example.sampledrawable;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView text1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        text1 = findViewById(R.id.textView);
     }
 }
